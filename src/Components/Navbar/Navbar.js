3f8e3ff0bac47fbfaf2addcd11e02255fbceda91
dragos <dragos042@yahoo.com>
2021-06-26 22:31:36 +0300

    progress

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Stories</li>
+        <li>Features</li>
+        <li>Pricing</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
