68aae861ccb8eb50610ce19a7e6262cd9531f6bc
dragos <dragos042@yahoo.com>
2021-07-16 22:56:25 +0300

    navbar progress

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,17 +1,22 @@
+import { useState } from 'react'
 import './Navbar.css'
-import sprite from '../../sprite.svg'
+import logo from './logo.svg'
+import menuIcon from './menu.svg'
+import closeIcon from './close.svg'
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false)
+
   return (
     <nav>
-      <svg className='logo'>
-        <use href={sprite + '#logo'} />
-      </svg>
+      <img src={logo} alt='logo' />
       <ul>
         <li>Stories</li>
         <li>Features</li>
         <li>Pricing</li>
       </ul>
+      <button>get an invite</button>
+      {menu ? <img src={menuIcon} /> : <img src={closeIcon} />}
     </nav>
   )
 }
