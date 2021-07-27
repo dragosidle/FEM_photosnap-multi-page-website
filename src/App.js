100c9c1a4f6c298aaa901ab6c98ac205a13bc712
dragos <dragos042@yahoo.com>
2021-07-27 23:59:08 +0300

    homepage progress

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css'
 // components
 import Navbar from './Components/Navbar/Navbar'
 import MobileMenu from './Components/MobileMenu/MobileMenu'
+import Homepage from './Components/Homepage/Homepage'
 
 function App() {
   const [open, setOpen] = useState(false)
@@ -19,27 +20,22 @@ function App() {
         <MobileMenu />
       </CSSTransition>
       <Navbar open={open} setOpen={setOpen} />
-      <main>
-        <article></article>
-      </main>
-      Create and share your photo Stories Photosnap is a platform for
-      photographers and visual storytellers. We make it easy to share photos,
-      tell stories and connect with others. Beautiful stories every time We
-      provide design templates to ensure your stories look terrific. Easily add
-      photos, text, embed maps and media from other networks. Then share your
-      story with everyone. View the stories Designed for everyone Photosnap can
-      help you create stories that resonate with your audience. Our tool is
-      designed for photographers of all levels, brands, businesses you name it.
-      View the stories The Mountains by John Appleseed Read Story Sunset
-      Cityscapes by Benjamin Cruz Read Story 18 Days Voyage by Alexei Borodin
-      Read Story Architecturals by Samantha Brooke Read Story 100% Responsive No
-      matter which the device you’re on, our site is fully responsive and
-      stories look beautiful on any screen. No Photo Upload Limit Our tool has
-      no limits on uploads or bandwidth. Freely upload in bulk and share all of
-      your stories in one go. Available to Embed Embed Tweets, Facebook posts,
-      Instagram media, Vimeo or YouTube videos, Google Maps, and more. Home
-      Stories Features Pricing Get an invite Copyright 2019. All Rights Reserved
-      Copyright 2019. All Rights Reserved
+      <Homepage />
+      Beautiful stories every time We provide design templates to ensure your
+      stories look terrific. Easily add photos, text, embed maps and media from
+      other networks. Then share your story with everyone. View the stories
+      Designed for everyone Photosnap can help you create stories that resonate
+      with your audience. Our tool is designed for photographers of all levels,
+      brands, businesses you name it. View the stories The Mountains by John
+      Appleseed Read Story Sunset Cityscapes by Benjamin Cruz Read Story 18 Days
+      Voyage by Alexei Borodin Read Story Architecturals by Samantha Brooke Read
+      Story 100% Responsive No matter which the device you’re on, our site is
+      fully responsive and stories look beautiful on any screen. No Photo Upload
+      Limit Our tool has no limits on uploads or bandwidth. Freely upload in
+      bulk and share all of your stories in one go. Available to Embed Embed
+      Tweets, Facebook posts, Instagram media, Vimeo or YouTube videos, Google
+      Maps, and more. Home Stories Features Pricing Get an invite Copyright
+      2019. All Rights Reserved Copyright 2019. All Rights Reserved
     </div>
   )
 }
---
+++ src/Components/Homepage/Homepage.js
@@ -0,0 +1,16 @@
+const Homepage = () => {
+  return (
+    <main>
+      <article>
+        <h1>Create and share your photo stories</h1>
+        <p>
+          Create and share your photo Stories Photosnap is a platform for
+          photographers and visual storytellers. We make it easy to share
+          photos, tell stories and connect with others.
+        </p>
+      </article>
+    </main>
+  )
+}
+
+export default Homepage
