5dcc2d4f9153867252d99274a8e0da623194056f
dragos <dragos042@yahoo.com>
2021-08-09 23:53:40 +0300

    cards fix

--- src/Components/RowPics/RowPics.js
+++ src/Components/RowPics/RowPics.js
@@ -6,26 +6,30 @@ import StoryCard from '../StoryCard/StoryCard'
 const RowPics = (props) => {
   return (
     <section className='row-pics'>
-      <StoryCard
-        Title={props.Title1}
-        Author={props.Author1}
-        RowImg={props.RowImg1}
-      />
-      <StoryCard
-        Title={props.Title2}
-        Author={props.Author2}
-        RowImg={props.RowImg2}
-      />
-      <StoryCard
-        Title={props.Title3}
-        Author={props.Author3}
-        RowImg={props.RowImg3}
-      />
-      <StoryCard
-        Title={props.Title4}
-        Author={props.Author4}
-        RowImg={props.RowImg4}
-      />
+      <div className='split'>
+        <StoryCard
+          Title={props.Title1}
+          Author={props.Author1}
+          RowImg={props.RowImg1}
+        />
+        <StoryCard
+          Title={props.Title2}
+          Author={props.Author2}
+          RowImg={props.RowImg2}
+        />
+      </div>
+      <div className='split'>
+        <StoryCard
+          Title={props.Title3}
+          Author={props.Author3}
+          RowImg={props.RowImg3}
+        />
+        <StoryCard
+          Title={props.Title4}
+          Author={props.Author4}
+          RowImg={props.RowImg4}
+        />
+      </div>
     </section>
   )
 }
