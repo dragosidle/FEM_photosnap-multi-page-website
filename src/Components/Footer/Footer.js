17cd9d26e6f6bd1b8ea92e0b0127203898692ec7
dragos <dragos042@yahoo.com>
2021-08-15 21:43:44 +0300

    footer progress
    missing hover states

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,32 +6,40 @@ import sprite from '../sprite.svg'
 // components
 import Logo from '../Logo/Logo'
 import NavLinks from '../NavLinks/NavLinks'
-
+import ArrowBtn from '../ArrowBtn/ArrowBtn'
 const Footer = () => {
   return (
     <section className='footer'>
-      <div className='split'>
-        <Logo />
+      <div className='footer-split'>
+        <Logo color={'white'} />
         <div className='socials'>
-          <svg>
+          <svg className='social-icon'>
             <use href={sprite + '#facebook'} />
           </svg>
-          <svg>
+          <svg className='social-icon'>
             <use href={sprite + '#youtube'} />
           </svg>
-          <svg>
+          <svg className='social-icon'>
             <use href={sprite + '#twitter'} />
           </svg>
-          <svg>
+          <svg className='social-icon'>
             <use href={sprite + '#pinterest'} />
           </svg>
-          <svg>
+          <svg className='social-icon'>
             <use href={sprite + '#instagram'} />
           </svg>
         </div>
       </div>
-      <NavLinks footer />
-      <div className='split'></div>
+      <NavLinks footer={true} styling={'footer-desktop'} />
+      <div className='footer-split-2'>
+        <ArrowBtn
+          btnType={'arrow-btn'}
+          btn={'get an invite'}
+          arrow={'white'}
+          font={'white'}
+        />
+        <h5 className='copyright'>Copyright 2019. All Rights Reserved</h5>
+      </div>
     </section>
   )
 }
--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,13 +1,11 @@
-// import logo from './logo.svg'
 import sprite from '../sprite.svg'
 
 const Logo = (props) => {
   return (
-    <svg style={{ fill: props.color, width: '370px' }}>
+    <svg style={{ fill: props.color, width: '170px', height: '50px' }}>
       <use href={sprite + '#logo'} />
     </svg>
   )
-  //  <img style={{ height: '16px' }} src={logo} alt='logo' />
 }
 
 export default Logo
