a4a8bd73ba06ac07d83c84fcf474e89640f9cbf9
dragos <dragos042@yahoo.com>
2021-08-25 01:05:32 +0300

    switch transition

--- src/Components/SwitchBtn/SwitchBtn.js
+++ src/Components/SwitchBtn/SwitchBtn.js
@@ -4,16 +4,24 @@ const SwitchBtn = (props) => {
   return (
     <div className='switch-container'>
       {props.trailing && (
-        <h5 style={{ opacity: props.monthly ? 0 : 1 }}>{props.trailing}</h5>
+        <h5 style={{ opacity: props.monthly ? 1 : 0.5 }}>{props.trailing}</h5>
       )}
       <div className='switch'>
         <div
           onClick={() => props.setMonthly(!props.monthly)}
-          className='switch-background'>
-          <div className='switch-circle'></div>
+          className='switch-background'
+          style={{ backgroundColor: props.monthly ? '#dfdfdf' : 'black' }}>
+          <div
+            style={{
+              backgroundColor: props.monthly ? 'black' : 'white',
+              right: props.monthly ? '30px' : '0px',
+            }}
+            className='switch-circle'></div>
         </div>
       </div>
-      {props.leading && <h5>{props.leading}</h5>}
+      {props.leading && (
+        <h5 style={{ opacity: props.monthly ? 0.5 : 1 }}>{props.leading}</h5>
+      )}
     </div>
   )
 }
