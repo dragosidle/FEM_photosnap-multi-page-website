b6faf1f04391b5d874a2d6430412ac7989e2e99d
dragos <dragos042@yahoo.com>
2021-08-28 23:39:32 +0300

    plan cards progress

--- src/Components/PricingCard/PricingCard.js
+++ src/Components/PricingCard/PricingCard.js
@@ -7,13 +7,15 @@ const PricingCard = (props) => {
   return (
     <div className={props.styling}>
       {props.colorThing && <div className='color-thing-card'></div>}
-      <div className='pricing-card--text'>
-        <h3>{props.title}</h3>
-        <p>{props.details}</p>
-      </div>
-      <div className='pricing-card--price'>
-        <h3>{props.price}</h3>
-        <p>{props.billing}</p>
+      <div className='plan-info'>
+        <div className='pricing-card--text'>
+          <h3>{props.title}</h3>
+          <p>{props.details}</p>
+        </div>
+        <div className='pricing-card--price'>
+          <h3>{props.price}</h3>
+          <p>{props.billing}</p>
+        </div>
       </div>
       <CtaButton styling={props.btn} buttonText={'pick plan'} />
     </div>
