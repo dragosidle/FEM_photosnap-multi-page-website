da543d88594ffa45cdc64a6c35190b01288f9730
dragos <dragos042@yahoo.com>
2021-08-30 22:04:44 +0300

    added table

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -1,19 +1,160 @@
 import './Table.css'
 
+// assets
+import sprite from '../sprite.svg'
+
 const Table = () => {
   return (
-    <table>
+    <table id='features'>
       <tr>
-        <th>The Features</th>
-        <th>Basic</th>
-        <th>Pro</th>
-        <th>Business</th>
+        <th>THE FEATURES</th>
+        <th>BASIC</th>
+        <th>PRO</th>
+        <th>BUSINESS</th>
       </tr>
       <tr>
-        <td>Unlimited Story Posting</td>
-        <td>yes</td>
-        <td>yes</td>
-        <td>yes</td>
+        <td>UNLIMITED STORY POSTING</td>
+        <td>
+          <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg>
+        </td>
+        <td>
+          <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg>
+        </td>
+        <td>
+          <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg>
+        </td>
+      </tr>
+      <tr>
+        <td>UNLIMITED PHOTO UPLOAD</td>
+        <td>
+          <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg>
+        </td>
+        <td>
+          <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg>
+        </td>
+        <td>
+          <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg>
+        </td>
+      </tr>
+      <tr>
+        <td>EMBEDDING CUSTOM CONTENT</td>
+        <td>
+          {/* <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg> */}
+        </td>
+        <td>
+          <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg>
+        </td>
+        <td>
+          <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg>
+        </td>
+      </tr>
+      <tr>
+        <td>CUSTOMIZE METADATA</td>
+        <td>
+          {/* <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg> */}
+        </td>
+        <td>
+          <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg>
+        </td>
+        <td>
+          <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg>
+        </td>
+      </tr>
+      <tr>
+        <td>ADVANCED METRICS</td>
+        <td>
+          {/* <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg> */}
+        </td>
+        <td>
+          {/* <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg> */}
+        </td>
+        <td>
+          <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg>
+        </td>
+      </tr>
+      <tr>
+        <td>PHOTO DOWNLOADS</td>
+        <td>
+          {/* <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg> */}
+        </td>
+        <td>
+          {/* <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg> */}
+        </td>
+        <td>
+          <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg>
+        </td>
+      </tr>
+      <tr>
+        <td>SEARCH ENGINE INDEXING</td>
+        <td>
+          {/* <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg> */}
+        </td>
+        <td>
+          {/* <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg> */}
+        </td>
+        <td>
+          <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg>
+        </td>
+      </tr>
+      <tr>
+        <td>CUSTOM ANALYTICS</td>
+        <td>
+          {/* <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg> */}
+        </td>
+        <td>
+          {/* <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg> */}
+        </td>
+        <td>
+          <svg className='table-check'>
+            <use href={sprite + '#check'} />
+          </svg>
+        </td>
       </tr>
     </table>
   )
